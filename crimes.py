51a71e4ca625403014bb93b5cb72210233878020
Kirk Strauser <kirk@strauser.com>
2014-06-22 14:55:22 -0700

    Added the BSD license header

--- crimes.py
+++ crimes.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2014, Kirk Strauser <kirk@strauser.com>
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import csv
 import os
 
@@ -115,6 +140,8 @@ def evaluate_city(city_name, state_name):
     key_lists = single_attributes[:]
     key_lists.append(['murder', 'aggravated assault', 'robbery', 'burglary', 'motor-vehicle-theft',
                       'larceny-theft'])
+    key_lists.append(['murder', 'aggravated assault', 'robbery'])
+    key_lists.append(VIOLENT_COLUMNS)
     for key_list in key_lists:
         for rank, row in sorted_by(key_list):
             if row['city'] == city_name and row['state'] == state_name:
